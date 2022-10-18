08d74fc64db1a584c78d5f5f5be73915a8f72033
Byeol <byeolkim1027@gmail.com>
2022-10-18 17:13:00 +0900

    풀페이지

---
+++ children/js/main.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+    const myFullpage = new fullpage('#fullpage', {  /* html에서 페이지 전체를 감싸는 요소 */
+
+		navigation: true, /* 오른쪽에 각 페이지의 paging */
+		navigationPosition: 'right', /* 위치 */
+		navigationTooltips: ['첫번째', '두번째', '세번째', '네번째'], /* 툴팁 */
+		showActiveTooltip: true, /* 현재 활성화된 페이지의 툴팁에 특정 클래스 주기 */
+
+		autoScrolling:true, /* 한페이지씩 스크롤 */
+		scrollHorizontally: true,
+
+		verticalCentered: true, /* 컨텐츠 요소 위아래 가운데 */
+
+		responsiveWidth: 640 /* fullpage를 적용시키지 않을 모바일 사이즈 */
+	});
+})
