79ac2744e0107725ebebb4b5c06537856bd6cb58
Byeol <byeolkim1027@gmail.com>
2022-10-20 17:04:09 +0900

    숫자변함,리스트반응형

--- children/js/main.js
+++ children/js/main.js
@@ -7,7 +7,7 @@ $(document).ready(function(){
 		navigation: true, /* 오른쪽에 각 페이지의 paging */
 		navigationPosition: 'right', /* 위치 */
 		navigationTooltips: ['메인', '보건통계', '사업소개', '현장소식', '지원사업'], /* 툴팁 */
-		showActiveTooltip: true, /* 현재 활성화된 페이지의 툴팁에 특정 클래스 주기/보이기 */
+		showActiveTooltip: false, /* 현재 활성화된 페이지의 툴팁에 특정 클래스 주기/보이기 */
 
 
         /* default 값이라서 안줘도 됨 */
@@ -24,17 +24,48 @@ $(document).ready(function(){
 				$('.header').removeClass('black')
 				$('#fp-nav').removeClass('black')
 			}
-			
+			if(destination.index==1){ //보건통계 슬라이드가 나타났을때(1은 그 화면을 나타냄)
+				funNumber() // 함수 이름이 funNumber 인것을 실행하는명령
+			}
 		},
         
         responsiveWidth: 640   /*fullpage를 적용시키지 않을 모바일 사이즈 */
         
+	});//fullpage finished
+
+	/*보건통계 숫자 카운팅 효과*/
+	function funNumber(){
+		console.log('움직임')
+		$('.numbers .rate ul li .num strong').counterUp()
+	}
+
+	/*현장소식에 swipe 효과주기*/
+	const swiper = new Swiper('.news .list', { /* 팝업을 감싼는 요소의 class명 */
+		slidesPerView: 1, /* 한번에 보일 팝업의 수 - 모바일 제일 작은 사이즈일때 */
+		spaceBetween: 16, /* 팝업과 팝업 사이 여백 */
+		breakpoints: {
+			500: {    /* 500px 이상일때 적용 */
+				slidesPerView: 2,
+				spaceBetween: 20,
+			},
+			800: {    /* 800px 이상일때 적용 */
+				slidesPerView: 3,
+				spaceBetween: 30,
+			},
+			1000: {    /* 1000px 이상일때 적용 */
+				slidesPerView: 4,
+				spaceBetween: 30,
+			},
+			1200: {    /* 1200px 이상일때 적용 */
+				slidesPerView: 4,
+				spaceBetween: 40,
+			},
+		},
+		pagination: {  /* 몇개의 팝업이 있는지 보여주는 동그라미 */
+			el: '.btn_paging', /* 해당 요소의 class명 */
+			clickable: true,  /* 클릭하면 해당 팝업으로 이동할 것인지 값 */
+		},
+
 	});
 
-	//count 숫자 count-up
-	// $('.numbers .rate ul li .num strong').counterUp({   /* 상세설정을 해주는 경우 */
-	// 	delay: 10,   /* 애니메이션이 작동되기 이전 대기 시간 */
-	// 	time: 2000    /* 전체 애니메이션 시간 */
-	// });
-
-})
+})//document.ready finished
