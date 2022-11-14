6d6ebf4499c8406b35ff8f4f1ec31d718ee33088
Byeol <byeolkim1027@gmail.com>
2022-11-14 18:08:07 +0900

    .

--- paradise/js/common.js
+++ paradise/js/common.js
@@ -1,4 +1,22 @@
 $(document).ready(function(){
+
+    let scrolling
+    scrollChk()
+
+    $(window).scroll(function(){
+        scrollChk()
+    })
+    function scrollChk(){
+        scrolling=$(window).scrollTop()
+        // console.log(scrolling,'스크롤 될때마다')
+
+        if(scrolling>0){ // 스크롤중(스크롤링이 숫자 0 보다 크면)
+            $('.header').addClass('fixed')
+        }else{ // 맨위로 이동
+            $('.header').removeClass('fixed')
+        }
+    }
+
     $('.header .tnb .lang .open').on('click',function(){
         $('.header .lang').addClass('lang_open')
     })
@@ -28,15 +46,52 @@ $(document).ready(function(){
         }
     })
 
-    $('.header .gnb>ul>li>a').on('mouseenter focusin',function(){
+    /*pc버전에서 메뉴에 마우스를 오버했을때, 자연스럽게 아래로 애니메이션 됬다, 없어졌다 하는 효과
+            ->원래는 addClass 로 클래스를 주고 햇었는데, slide up/down 으로 자연스럽게 효과를 준다*/
+    $('.header .gnb>.gnb_wrap>ul>li>a').on('mouseenter focusin',function(){
         if(pcMobile=='pc'){
+            $('.header .gnb>.gnb_wrap>ul>li>ul').slideDown()
             $('.header').addClass('menu_open')
         }    
     })
     $('.header').on('mouseleave',function(){
-        $('.header').removeClass('menu_open')
+        if(pcMobile=='pc'){
+            $('.header .gnb>.gnb_wrap>ul>li>ul').slideUp(200)//빨리 슬라이드 되서 사라지는 시간대(0.2초)
+            $('.header').removeClass('menu_open')
+        }    
     })
-    $('.header .gnb>ul>li:last-child>ul>li:last-child>a').on('focusout',function(){
-        $('.header').removeClass('menu_open')
+
+    let gnbStu 
+    $('.header .gnb .gnb_open').on('click',function(){
+        gnbStu=$('.header .gnb').hasClass('mobile_open')
+        if(gnbStu==true){ //열린상태 - 닫는기능
+            $('.header .gnb').removeClass('mobile_open')
+            $('.header .logo').show()//메뉴가 클릭됬을때사라지는 요소들
+            $('.visual .popup ul li .txt').show()
+            $('.visual .popup .scroll').show()
+        }else{ //닫힌상태 - 여는 기능
+            $('.header .gnb').addClass('mobile_open')
+            $('.header .logo').hide()
+            $('.visual .popup ul li .txt').hide()
+            $('.visual .popup .scroll').hide()
+        }
     })
+
+    $('.header .gnb>.gnb_wrap>ul>li>a').on('click', function(e){
+        if(pcMobile=='mobile'){
+            e.preventDefault()
+            /* 1차 메뉴를 클릭했을때 a href 로 페이지가 자동으로 이동하는 현상 막기*/
+            let menu_st=$(this).parents('li').hasClass('sub_open')
+            if(menu_st==true){
+                $(this).next().slideUp()
+                $(this).parents('li').removeClass('sub_open')
+            }else{
+                $(this).next().slideDown()
+                $(this).parents('li').addClass('sub_open')
+            }
+            // $(this).parents('li').toggleClass('sub_open')
+        }
+        
+    })
+
 })
