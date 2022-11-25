691275a9f06182ae6b756eea008708f735cc9dc2
Byeol <byeolkim1027@gmail.com>
2022-11-25 13:11:30 +0900

    portfolio

--- portfolio/js/main.js
+++ portfolio/js/main.js
@@ -1,29 +1,4 @@
 $(document).ready(function(){
-    // let typingBool = false; 
-    // let typingIdx=0; 
-
-    // // 타이핑될 텍스트를 가져온다 
-    // let typingTxt = $(".typing_txt").text(); 
-
-    // typingTxt=typingTxt.split(""); // 한글자씩 자른다. 
-
-    // if(typingBool==false){ 
-    //     // 타이핑이 진행되지 않았다면 
-    //     typingBool=true;     
-    //     var tyInt = setInterval(typing,200); // 반복동작 
-    //     } 
-            
-    //     function typing(){ 
-    //     if(typingIdx<typingTxt.length){ 
-    //         // 타이핑될 텍스트 길이만큼 반복 
-    //         $(".typing").append(typingTxt[typingIdx]);
-    //         // 한글자씩 이어준다. 
-    //         typingIdx++; 
-    //     } else{ 
-    //         //끝나면 반복종료 
-    //         clearInterval(tyInt); 
-    //     } 
-    // }  
     const content = "안녕하세요, 웹퍼블리셔 김별입니다.";
     const text = document.querySelector(".txt");
     let i = 0;
@@ -37,4 +12,11 @@ $(document).ready(function(){
         }
     }
     setInterval(typing, 200)
+
+    $(document).ready(function($){
+        $(".scroll_move").click(function(event){         
+                event.preventDefault();
+                $('html,body').animate({scrollTop:$(this.hash).offset().top}, 200);
+        })
+    })
 })
